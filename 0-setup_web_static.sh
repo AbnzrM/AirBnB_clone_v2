dc1c52784aed7e93fb7bec189aeb53643387ffff
Abnzr <abeni350@gamil.com>
2022-10-30 01:32:10 -0800

    AirBnB_clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
-# Web servers setup for the deployment of web_static
+# sets up the web servers for the deployment of web_static
 
-sudo apt-get update -y
-sudo apt-get install nginx -y
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo echo -e "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-update="\\\n\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}"
-sudo sed -i "50i $update" /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
